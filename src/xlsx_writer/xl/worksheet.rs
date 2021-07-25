0574c94a3058aaee47504934d511dfcccfb5ee2d
Ferdinand Schäffler <de.frankenapps@gmail.com>
2021-07-25 17:32:29 +0200

    Format and fix error in core.rs

--- src/xlsx_writer/xl/worksheet.rs
+++ src/xlsx_writer/xl/worksheet.rs
@@ -1,15 +1,15 @@
 pub fn get_worksheet_xml() -> String {
     String::from(
         r#"<?xml version="1.0" encoding="utf-8" standalone="yes"?>
-        <worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
-            <sheetData>
-                <row r="1" ht="27.65" customHeight="1">
-                    <c r="A1" t="s" s="2">
-                        <v>0</v>
-                    </c>
-                </row>
-            </sheetData>
-        </worksheet>
+<worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
+    <sheetData>
+        <row r="1">
+            <c r="A1" t="s" s="2">
+                <v>0</v>
+            </c>
+        </row>
+    </sheetData>
+</worksheet>
         "#
     )
 }
